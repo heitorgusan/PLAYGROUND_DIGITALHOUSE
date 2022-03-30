720a58ec2f65225e0a62a8958f600e86fd39dcd9
heitorgusan <heitorgusan@gmail.com>
2022-03-29 23:09:30 -0300

    Stream

--- Playground/src/Stream/Filter.java
+++ Playground/src/Stream/Filter.java
@@ -1,10 +1,37 @@
 package Stream;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Filter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		///////////////////1. Streams filter() and collect()/////////////////////////////////////
+		//Before Java 8
+		List<String> listaTech = Arrays.asList("Node","Spring","React");
+		List<String> resultListaTech = getFilterOutput(listaTech,"Spring");
+		
+		for(String item: resultListaTech) {
+			System.out.println(item);
+		}
+		
+		
+		
+		
+		
 	}
-
-}
+	
+	
+	public static List<String> getFilterOutput(List<String>listaString, String filter){
+		List<String> result = 	new ArrayList<>();
+		
+		for(String item : listaString) {
+			if(!item.equals(filter)) {
+				result.add(item);
+				}
+			}
+		return result;
+		
+		}
+	}
