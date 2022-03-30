2593deec25a1baf6f1dd895d7fac140babf35c7b
heitorgusan <heitorgusan@gmail.com>
2022-03-29 22:36:55 -0300

    More map knowledged

--- Playground/src/Stream/Map.java
+++ Playground/src/Stream/Map.java
@@ -10,6 +10,7 @@ public class Map {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//A List of Strings to Uppercase ////////////////////////////////////////////////////////////////////////////
 		//Before Java 8
 		List<String>alpha = Arrays.asList("a","b","c","d","e","f","g","h");
 		List<String>alphaUpper = new ArrayList<>();
@@ -29,7 +30,7 @@ public class Map {
 		List<Integer> collectNumDobrar = num.stream().map(n -> n*2).collect(Collectors.toList());
 		System.out.println(collectNumDobrar); // 2 , 4 ,6 ,8 ,10
 		
-		// List of objects -> List of String
+		// List of objects -> List of String//////////////////////////////////////////////////////////////////////////////////////////
 		
 		List<Staff> staff = Arrays.asList(
 				new Staff("Heitorzin",24,new BigDecimal(10000)),
@@ -48,7 +49,62 @@ public class Map {
 		List<String> collectName = staff.stream().map(objFromStaff -> objFromStaff.getName()).collect(Collectors.toList());
 		System.out.println(collectName);
 		
+		//List of objects -> List of other objects//////////////////////////////////////////////////////////////////////////////////////////
 		
+		//Before 8
+		
+		List<Staff> staff2 = Arrays.asList(
+                new Staff("mkyong", 30, new BigDecimal(10000)),
+                new Staff("jack", 27, new BigDecimal(20000)),
+                new Staff("lawrence", 33, new BigDecimal(30000))
+        );
+
+        List<StaffPublic> result2 = convertToStaffPublic(staff2);
+        //System.out.println(result2.get(0).getName());
+        System.out.println(result2);
+        
+        
+        //Java 8
+        
+        List<Staff> staff3 = Arrays.asList(
+        		new Staff("mkyong", 30, new BigDecimal(10000)),
+                new Staff("jack", 27, new BigDecimal(20000)),
+                new Staff("lawrence", 33, new BigDecimal(30000))
+        		
+        		
+        );
+        
+        List<StaffPublic> result3 = staff.stream().map(temp -> {
+            StaffPublic obj = new StaffPublic();
+            obj.setName(temp.getName());
+            obj.setAge(temp.getAge());
+            if ("mkyong".equals(temp.getName())) {
+                obj.setExtra("this field is for mkyong only!");
+            }
+            return obj;
+        }).collect(Collectors.toList());
+
+        System.out.println(result3);
+		
+	}
+	
+	public static List<StaffPublic> convertToStaffPublic(List<Staff> staff) {
+		List<StaffPublic> result = new ArrayList<>();
+		
+		for (Staff temp : staff) {
+
+            StaffPublic obj = new StaffPublic();
+            obj.setName(temp.getName());
+            obj.setAge(temp.getAge());
+            if ("mkyong".equals(temp.getName())) {
+                obj.setExtra("this field is for mkyong only!");
+            }
+
+            result.add(obj);
+        }
+
+        return result;
+
 	}
 
 }
