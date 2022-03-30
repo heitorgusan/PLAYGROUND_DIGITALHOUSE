23ddd418910dfc3237c88509d72502b48d961be2
heitorgusan <heitorgusan@gmail.com>
2022-03-29 23:23:55 -0300

    Stream Java8

--- Playground/src/Stream/Filter.java
+++ Playground/src/Stream/Filter.java
@@ -3,6 +3,7 @@ package Stream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Filter {
 
@@ -11,13 +12,17 @@ public class Filter {
 		//Before Java 8
 		List<String> listaTech = Arrays.asList("Node","Spring","React");
 		List<String> resultListaTech = getFilterOutput(listaTech,"Spring");
-		
-		for(String item: resultListaTech) {
-			System.out.println(item);
-		}
+		System.out.println(resultListaTech);
 		
 		
+		//Now Java8
 		
+		List<String> listaTech2 = Arrays.asList("Node","Spring","C#","Python");
+		List<String>result2ListaTech = listaTech2.stream().
+				filter(item -> !"Python".equals(item)).
+				collect(Collectors.toList());
+				
+		System.out.println(result2ListaTech);
 		
 		
 	}
