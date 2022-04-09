564ea7e55fe5c1301e3df3b621292f1ff915f16b
heitorgusan <heitorgusan@gmail.com>
2022-04-08 21:25:09 -0300

    mudança bem simples

---
+++ Playground/src/Collection/Exercicios/E2_APELIDOS.java
@@ -0,0 +1,23 @@
+package Collection.Exercicios;
+/*
+Criar um dicionário que represente os apelidos que uso para chamar os
+meus amigos. Para isso, o dicionário deverá ter como chave String e como
+valor uma lista de Strings.
+
+João → Juan, Fissura, Maromba
+Miguel → Night Watch, Bruce Wayne, Tampinha
+Maria → Wonder Woman, Mary, Marilene
+Lucas → Lukinha, Jorge, George
+
+Em seguida crie uma classe Exercício 1 com seu método main, imprimir no Main
+todas as chaves junto com seus valores associados. Para isso, utilizar o
+foreach com as chaves.
+*/
+public class E2_APELIDOS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- Playground/src/Stream/ForEach.java
+++ Playground/src/Stream/ForEach.java
@@ -7,7 +7,7 @@ public class ForEach {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Stream<Integer> streamDadosNumericos = Stream.of(1,2,3,4,5);
+		Stream<Integer> streamDadosNumericos = Stream.of(1,2,3,4,5,6,7,8);
 		
 		streamDadosNumericos.forEach(System.out::println);
 
